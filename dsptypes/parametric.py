Emigon/dsptypes

commit d4940813c2970f555350129152ba5163d86197e6

fixes bug from update to plotting_styles

dsptypes/signal1D.py
@@ -183,7 +183,7 @@ class Signal1D(object):
         if xunits is not None:
             xaxis = xaxis.to(xunits)
 
-        line, = plt.plot(xaxis, plotting_styles[style](self.values))
+        line, = plt.plot(xaxis, plotting_styles[style](self._z))
         plt.ylabel(style)
         return line
 

commit 3066ec3da6de50b7685028e1c0a010dd0346d679

Fix bug in Parametric1D introduced in d4940813

dsptypes/parametric.py
@@ -233,10 +233,10 @@ class Parametric1D(object):
             if fft:
                 trace = trace.fft()
 
-            line.set_ydata(plotting_styles[self._gui_style](trace.values))
+            line.set_ydata(plotting_styles[self._gui_style](trace))
 
             for sigma, line in psigs:
-                line.set_ydata(plotting_styles[self._gui_style](sigma.values))
+                line.set_ydata(plotting_styles[self._gui_style](sigma))
 
             axtop.relim()
             axtop.autoscale_view()
@@ -303,7 +303,7 @@ class Parametric1D(object):
                 if fft:
                     trace = trace.fft()
 
-                line.set_ydata(plotting_styles[self._gui_style](trace.values))
+                line.set_ydata(plotting_styles[self._gui_style](trace))
                 ax2.relim()
                 ax2.autoscale_view()
 

dsptypes/signal1D.py
@@ -20,13 +20,13 @@ from dsptypes import *
 
 plotting_styles = \
     {
-        'dB':   lambda z: 10*np.log10(np.abs(z.to_numpy())),
-        'dBm':  lambda z: 10*np.log10(np.abs(z.to_numpy())) + 30,
-        'abs':  lambda z: np.abs(z.to_numpy()),
-        'rad':  lambda z: np.angle(z.to_numpy()),
-        'deg':  lambda z: np.deg2rad(np.angle(z.to_numpy())),
-        'real': lambda z: np.real(z.to_numpy()),
-        'imag': lambda z: np.imag(z.to_numpy()),
+        'dB':   lambda z: 10*np.log10(np.abs(z.values)),
+        'dBm':  lambda z: 10*np.log10(np.abs(z.values)) + 30,
+        'abs':  lambda z: np.abs(z.values),
+        'rad':  lambda z: np.angle(z.values),
+        'deg':  lambda z: np.deg2rad(np.angle(z.values)),
+        'real': lambda z: np.real(z.values),
+        'imag': lambda z: np.imag(z.values),
     }
 
 def return_copy(name):
@@ -183,7 +183,7 @@ class Signal1D(object):
         if xunits is not None:
             xaxis = xaxis.to(xunits)
 
-        line, = plt.plot(xaxis, plotting_styles[style](self._z))
+        line, = plt.plot(xaxis, plotting_styles[style](self))
         plt.ylabel(style)
         return line
 
@@ -194,11 +194,11 @@ class Signal1D(object):
         plt.ylabel('Im')
 
     def samples_above(self, val, tform = 'real'):
-        idxs, = np.where(plotting_styles[tform](self._z) > val)
+        idxs, = np.where(plotting_styles[tform](self) > val)
         return Signal1D(self._z.values[idxs], xraw = self.x[idxs])
 
     def samples_below(self, val, tform = 'real'):
-        idxs, = np.where(plotting_styles[tform](self._z) < val)
+        idxs, = np.where(plotting_styles[tform](self) < val)
         return Signal1D(self._z.values[idxs], xraw = self.x[idxs])
 
     @property