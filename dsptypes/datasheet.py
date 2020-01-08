bace329b04930592605e3b41a9d03d3fcbc2a363
Daniel Parker <danielparker@live.com.au>
2020-01-08 15:03:16 +1100

    Implement SNR vs metric datasheet methods and plot
    This commit introduces a core plot that will assist in the development
    of fitting procedures: the SNR boxplot, implemented under snr_boxplot.
    
    For a user defined metric, snr_boxplot will produce a series of boxplots
    for different levels of specified signal to noise ratio. This allows the
    user to easily observe fitting method performance over the entire
    parameter space and observe how it is affected by signal to noise ratio.

--- dsptypes/datasheet.py
+++ dsptypes/datasheet.py
@@ -7,6 +7,8 @@ Parametric1D types
 """
 
 import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
 
 def sample(pm1d, N, x, **callkwargs):
     """ sample pm1d(x) for N uniformly sampled points in the parameter space """
@@ -14,3 +16,68 @@ def sample(pm1d, N, x, **callkwargs):
         for p in pm1d.v:
             pm1d.v[p] = np.random.uniform(low = pm1d.v._l[p], high = pm1d.v._u[p])
         yield pm1d(x, **callkwargs)
+
+def apply_metric_to(sampler, metric):
+    """ evalute metric on samples drawn from sampler
+
+    Args:
+        sampler:        a python generator that yields Signal1Ds
+        metric:         a function that accepts a Signal1D and returns a
+                        (metric_value, metadata) pair, whereby metric_value is some
+                        positive number and the metadata object is a consistent
+                        pd.Series object containing additional information to log
+    Returns:
+        table:          pd.DataFrame with columns containing the metadata labels +
+                        the metric_value for each sample. indices are integers
+    """
+    rows = []
+    for sig1d in sampler:
+        val, metadata = metric(sig1d)
+        metadata['metric'] = val
+        rows.append(metadata)
+
+    return pd.DataFrame(rows)
+
+def snr_sweep(pm1d, x, metric, snrs, N, **callkwargs):
+    """ apply_metric_to samples drawn from pm1d at the specified signal to noises
+
+    Args:
+        pm1d:       The Parametric1D model to draw samples from
+        x:          The x-axis to evaluate the model over
+        metric:     The metric to apply to each of the samples
+        snrs:       A list of signal to noise ratios to draw samples with
+        N:          The number of samples to draw for each signal to noise ratio
+        callkwargs: The keyword arguments to pass to Parametric1D.__call__
+
+    Returns:
+        table:      Same as the table returned by apply_metric_to but with an
+                    additional 'snr' column
+    """
+    tables = []
+    for snr in snrs:
+        callkwargs['snr'] = snr
+        sampler = sample(pm1d, N, x, **callkwargs)
+        table = apply_metric_to(sampler, metric)
+        table['snr'] = N*[snr]
+        tables.append(table)
+
+    return pd.concat(tables, ignore_index = True)
+
+def snr_boxplot(pm1d, x, metric, snrs, N, **callkwargs):
+    """ plot the distribution of metric values vs snr as a series of box plots
+
+    Args:   same as datasheet.snr_sweep
+    """
+    dataset = snr_sweep(pm1d, x, metric, snrs, N)
+
+    # subdivide the data
+    folded = np.array([dataset[dataset.snr == snr].metric.values for snr in snrs])
+
+    w = np.diff(snrs).min()/2
+    plt.boxplot(folded.T, positions = snrs, widths = len(snrs)*[w])
+    plt.xlabel('SNR (dB)')
+    plt.ylabel('Metric')
+    plt.ylim(-1e-2*dataset.metric.max())
+    plt.xlim(np.min(snrs) - .6*w, np.max(snrs) + .6*w)
+
+    return dataset
--- test/test_datasheet.py
+++ test/test_datasheet.py
@@ -18,8 +18,50 @@ def model():
 
     return Parametric1D(sp.exp(tau*x) + alpha, parameters)
 
+def test_apply_metric_to(model):
+    def lsq_metric(sig1d):
+        metadata = model.fitshgo(sig1d, n = 1, iters = 1)
+        return model(sig1d.x)@sig1d, metadata
+
+    x = np.linspace(0, 1, 100)
+    table = apply_metric_to(sample(model, 4, x, snr = 20) , lsq_metric)
+
+    assert len(table) == 4
+    for col in table.columns:
+        assert col in ['metric', 'parameters', 'fitted', 'opt_result']
+
+def test_snr_sweep(model):
+    def lsq_metric(sig1d):
+        metadata = model.fitshgo(sig1d, n = 1, iters = 1)
+        return model(sig1d.x)@sig1d, metadata
+
+    x = np.linspace(0, 1, 100)
+    table = snr_sweep(model, x, lsq_metric, [15, 20], 4)
+
+    assert len(table) == 8
+    for col in table.columns:
+        assert col in ['metric', 'snr', 'parameters', 'fitted', 'opt_result']
+
+    for snr in table.snr.values:
+        assert snr in [15, 20]
+
 @pytest.mark.plot
 def test_sample(model):
     for sigma in sample(model, 10, np.linspace(0, 1, 100), snr = 20):
         sigma.plot()
+    plt.show()
+
+@pytest.mark.plot
+def test_snr_boxplot(model):
+    def dummy_metric(sig1d):
+        return np.abs(np.random.normal()), pd.Series({'fitted': sig1d})
+
+    x = np.linspace(0, 1, 24)
+
+    fig, axes = plt.subplots()
+    plt.sca(axes)
+
+    data = snr_boxplot(model, x, dummy_metric, [12, 15, 18, 21], 100)
+
+    fig.tight_layout()
     plt.show()
