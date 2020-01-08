649195631e883cb933465127f4010a0cb5e24a2d
Daniel Parker <danielparker@live.com.au>
2020-01-08 12:03:14 +1100

    supresses pint BehaviourChangeWarning

--- dsptypes/__init__.py
+++ dsptypes/__init__.py
@@ -1,3 +1,5 @@
+import warnings
+
 from pint import UnitRegistry, set_application_registry
 
 ureg = UnitRegistry()
@@ -7,6 +9,10 @@ set_application_registry(ureg)
 ureg.define('samples = 1') # for unspecified units
 
 Q_ = ureg.Quantity
+
+with warnings.catch_warnings():
+  warnings.simplefilter("ignore")
+  Q_([])
 
 from .parametric import *
 from .signal1D import *
